847c2029fa3de65bb330d3e27b775046b8df2f3b
Larry Ruckman <ruckman@slac.stanford.edu>
2024-07-25 11:40:13 -0700

    changing IP address

--- firmware/targets/test_kv260/CreatePetalinuxProject.sh
+++ firmware/targets/test_kv260/CreatePetalinuxProject.sh
@@ -2,7 +2,7 @@
 ####################################################
 
 # Release the petalinux with static IP address
-export SOC_IP_STATIC="10.0.0.10"
+export SOC_IP_STATIC="192.168.0.2"
 
 # Define the hardware type
 # Note: Must match the axi-soc-ultra-plus-core/hardware directory name
